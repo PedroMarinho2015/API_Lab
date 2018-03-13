4bc4ceeba05c7dda7391a7e2c6775055d68b90db
HafsaMire <35596970+HafsaMire@users.noreply.github.com>
2018-03-13 13:27:53 +0100

    Update app.js

--- app.js
+++ app.js
@@ -29,7 +29,8 @@ work1: function(my) { this.led1.turnOn(); },
 work2: function(my) { this.led1.turnOff(); },
 work3: function(my) { this.led2.turnOn(); },
 work4: function(my) { this.led2.turnOff(); },
-work5: function(my) { every((20).second(), this.led3.turnOn()); 
+work5: function(my) { every((20).second(), this.led3.toggle()); 
+work6: function(my) { this.led3.turnOff()                     
 },
 
 });
